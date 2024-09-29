9f8e12289be1b8797243b0eb296e96b590f9ffd1
zhouzhe <zzhe828@gmail.com>
2024-09-30 01:49:44 +0800

    成功了嗎？

---
+++ petlove.sql
@@ -0,0 +1,8 @@
+CREATE TABLE userlogin (
+    user_id INT PRIMARY KEY NOT NULL ,
+    user_name VARCHAR(10) NOT NULL,
+    user_phone VARCHAR(10) NOT NULL,
+    user_mail VARCHAR(30) NOT NULL,
+    user_password VARCHAR(20) NOT NULL
+    )
+
